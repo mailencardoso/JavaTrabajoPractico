270e7f04552f55a70e99d77e5054e27c6d240755
Mailen <mailen.cardoso@yahoo.com.ar>
2020-07-28 23:16:14 -0300

    Template de bootstrap y estructura de proyecto.

---
+++ src/java/Negocio/Usuario.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Usuario {
+    String usuario;
+    String contraseña;
+    String tipoUsuario;
+    String nombreUsuario;
+    String apellidoUsuario;
+    String telefono;
+    String mail;
+    String direccion;
+    
+    public Usuario(String usu, String con, String tipoUsu, String nombre, String apellido, String telefono, String mail, String dir) {
+        this.usuario = usu;
+        this.contraseña = con;
+        this.tipoUsuario = tipoUsu;
+        this.nombreUsuario = nombre;
+        this.apellidoUsuario = apellido;
+        this.telefono = telefono;
+        this.mail = mail;
+        this.direccion = dir;
+    }
+    
+    public Usuario (){
+    }
+    
+    pr
+}
