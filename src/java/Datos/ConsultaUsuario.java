fed079c723c8094785dc5f8d8bc5da8366d3ebe7
Gaston Albizuri <gastonalbizuri@hotmail.com>
2020-08-05 00:39:42 -0300

    se completó ABM de usuarios(ver direcciones)
    Ver cómo vamos a definir las direcciones de los usuarios

--- src/java/Datos/ConsultaUsuario.java
+++ src/java/Datos/ConsultaUsuario.java
@@ -32,12 +32,114 @@ public class ConsultaUsuario extends Conexion{
         ResultSet rs = null;
         Usuario usuarioActual = new Usuario();
         
-        /*falta agregar codigo*/
-        
-        
-        
+         try {
+            String query = "SELECT * FROM Usuario WHERE usuario = ? AND pass = ?";
+            pst = getConexion().prepareStatement(query);
+            pst.setString(1, usuario);
+            pst.setString(2, pass);
+            rs = pst.executeQuery();
+            
+            if (rs.absolute(1)){
+                usuarioActual.setUsuario(rs.getString("usuario"));
+                usuarioActual.setPassword(rs.getString("pass"));
+                usuarioActual.setNombre(rs.getString("nombre"));
+                usuarioActual.setApellido(rs.getString("apellido"));
+                usuarioActual.setTipoUsuario(rs.getString("tipo_usuario"));
+                usuarioActual.setTelefono(rs.getString("telefono"));
+                usuarioActual.setMail(rs.getString("email"));
+                
+                return usuarioActual;
+            }
+        } catch (SQLException e) {
+            error = "Error: "+e;
+        } finally{
+            try {
+                 if (getConexion()!= null) getConexion().close();
+                 if (pst!=null) pst.close();
+                 if (rs!= null) rs.close();
+            } catch (SQLException e) {
+                error = "Error: "+e;
+            }   
+        }
+        usuarioActual=null;     
         return usuarioActual;
     }  
     
+    public boolean agregarCliente(String usuario, String contraseña, String tipoUsuario, String nombre, String apellido, String telefono, String mail, String calle, int numero, String tipoDireccion, int piso, int departamento, String bloque, String comentarios) {
+        PreparedStatement pst= null;
+        try {
+            getConexion().setAutoCommit(false);
+            String query= "INSERT INTO Usuario (usuario,pass,tipo_usuario,nombre,apellido,telefono,email) VALUES (?,?,?,?,?,?,?);";
+            pst = getConexion().prepareStatement(query);
+            pst.setString(1, usuario);
+            pst.setString(2, contraseña);
+            pst.setString(3, tipoUsuario);
+            pst.setString(4, nombre);
+            pst.setString(5, apellido);
+            pst.setString(6, telefono);
+            pst.setString(7, mail);
+            int ban=pst.executeUpdate();
+            
+            /*VER QUE HACEMOS CON DIRECCIONES*/
+            String query2= "INSERT INTO direcciones (calle_direccion,numero_direccion,tipo_direccion,piso_direccion,departamento_direccion,bloque_direccion,usuario_direccion,comentarios) VALUES (?,?,?,?,?,?,?,?);";
+            pst = getConexion().prepareStatement(query2);
+            pst.setString(1, calle);
+            pst.setInt(2, numero);
+            pst.setString(3, tipoDireccion);
+            pst.setInt(4, piso);
+            pst.setInt(5, departamento);
+            pst.setString(6, bloque);
+            pst.setString(7, usuario);
+            pst.setString(8, comentarios);
+            int ban2=pst.executeUpdate();
+            
+            getConexion().commit();
+            if(ban==1 && ban2==1){
+                return true;
+            }
+        } catch (SQLIntegrityConstraintViolationException e) {
+            error = "Error: Nombre de usuario existente";
+        } catch(SQLException e){
+            error= "Error: "+e;
+        }finally {
+            try {
+                if (getConexion()!=null) getConexion().close();
+                if (pst!=null) pst.close();
+            } catch (SQLException e) {
+                error = "Error: "+e;
+            }
+        }
+        return false;
+                
+    }
+    public boolean modifUsuario(String usuario, String nombre, String apellido, String telefono, String mail, String contraseña, String tipoUsuario) {
+        PreparedStatement pst = null;
+        ResultSet rs= null;
+        try {
+            pst = getConexion().prepareStatement("UPDATE Usuario SET pass=?, tipo_usuario=?, nombre =?, apellido=?, telefono=?,email=? WHERE usuario = ?");
+            pst.setString(1,contraseña);
+            pst.setString(2,tipoUsuario);
+            pst.setString(3,nombre);
+            pst.setString(4,apellido);
+            pst.setString(5,telefono);
+            pst.setString(6,mail);
+            pst.setString(7,usuario);
+            
+            if(pst.executeUpdate()==1){
+                return true;
+            }
+        } catch (SQLException e) {
+            error = "Error: "+e;
+        } finally{
+            try{
+                if (pst!=null) pst.close();
+                if (getConexion()!=null) getConexion().close();
+                if (rs!= null) rs.close();
+            } catch(SQLException e) {
+                error = "Error: "+e;
+                }
+        }
+        return false;
+    }
          
 }
